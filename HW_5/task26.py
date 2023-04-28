93991c9c17f0ce0ad0349f0e9522da4a09066a65
khamitov <khamitov.aleksandr@gmail.com>
2023-04-28 16:03:24 +0200

    add: change def

--- HW_5/task26.py
+++ HW_5/task26.py
@@ -5,13 +5,18 @@
 
 # A=3; B = 5 -> 243 (3⁵)
 #    A = 2; B = 3 -> 8
+import math
 
 
 def power(base, exp):
-    if (exp == 1):
-        return (base)
-    if (exp != 1):
-        return (base * power(base, exp - 1))
+    if exp == 0:
+        return 1
+    if exp < 0:
+        return 1 / power(base, -exp)
+    if exp % 2 == 0:
+        return power(base, exp // 2) * power(base, exp // 2)
+    else:
+        return power(base, exp - 1) * base
 
 
 base = int(input("Enter the number: "))
