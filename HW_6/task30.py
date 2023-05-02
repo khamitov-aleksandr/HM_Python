106d69820ec2895f2cdfeac8700ec894cc93e2aa
khamitov <khamitov.aleksandr@gmail.com>
2023-05-02 11:01:57 +0200

    add: task30.py chanche description

--- HW_6/task30.py
+++ HW_6/task30.py
@@ -1,6 +1,6 @@
 # Task 30: Fill an array with elements of an arithmetic progression.
-# Its first element, difference and number of elements must be entered from the keyboard.
-# The formula for getting the nth member of the progression is: an = a1 + (n-1) * d.
+# Its first element, difference, and a number of elements must be entered from the keyboard.
+# The formula for getting the nth member of the progression is an = a1 + (n-1) * d.
 # Each number is entered on a new line.
 
 a1 = int(input("Enter first element: "))
